09d48bcfff26f87a388cb8b2d8e6a393e8622365
fm946969 <fm946969@hotmail.com>
2025-09-13 22:27:44 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,183 @@
+function _0302 () {
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(330, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+}
+function _07 () {
+    _0701()
+    _0702()
+    _0703()
+}
+function _01 () {
+    _0101()
+    _0102()
+    _0103()
+}
+function _03 () {
+    _0301()
+    _0302()
+    _0303()
+}
+function _0301 () {
+    music.play(music.tonePlayable(311, music.beat(BeatFraction.Whole) + music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+}
+function _0602 () {
+    music.play(music.tonePlayable(330, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(294, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+}
+function _0401 () {
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(165, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(165, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+}
+function _0503 () {
+    music.play(music.tonePlayable(294, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+}
+function _06 () {
+    _0601()
+    _0602()
+    _0603()
+}
+function _0701 () {
+    _0101()
+}
+function _0201 () {
+    music.play(music.tonePlayable(165, music.beat(BeatFraction.Half) + music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(165, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+}
+function _02 () {
+    _0201()
+    _0202()
+    _0203()
+}
+input.onButtonPressed(Button.A, function () {
+    music.setTempo(90)
+    _01()
+    _02()
+    _03()
+    _04()
+    _05()
+    _06()
+    _07()
+    _08()
+})
+function _0403 () {
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole) + music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+}
+function _0501 () {
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole) + music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+}
+function _04 () {
+    _0401()
+    _0402()
+    _0403()
+}
+function _0402 () {
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Whole) + music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(294, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+}
+function _0102 () {
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole) + music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+}
+function _0103 () {
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(185, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(165, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+}
+function _0203 () {
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+}
+function _0502 () {
+    music.play(music.tonePlayable(208, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Whole))
+}
+function _0603 () {
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+}
+function _0702 () {
+    music.play(music.tonePlayable(185, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(208, music.beat(BeatFraction.Whole) + music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+}
+function _0801 () {
+    music.play(music.tonePlayable(208, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(185, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+}
+function _0303 () {
+    music.play(music.tonePlayable(330, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(294, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+}
+function _0703 () {
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole) + music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(185, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(165, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+}
+function _0802 () {
+    music.play(music.tonePlayable(294, music.beat(BeatFraction.Whole) + music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+}
+function _0803 () {
+    music.play(music.tonePlayable(208, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+}
+function _0101 () {
+    music.play(music.tonePlayable(165, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole) + music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(185, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+}
+function _0601 () {
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(165, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Whole))
+}
+function _05 () {
+    _0501()
+    _0502()
+    _0503()
+}
+function _08 () {
+    _0801()
+    _0802()
+    _0803()
+}
+function _0202 () {
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole) + music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(311, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(311, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+}
